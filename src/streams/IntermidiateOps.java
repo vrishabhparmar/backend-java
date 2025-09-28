ca737efd8a4e871cc0ceadb0ced25e889a85bb57
vrishabhparmar <vrishabhluvcars@gmail.com>
2025-09-28 16:44:21 +0530

    Intermidiate Op

--- src/streams/IntermidiateOps.java
+++ src/streams/IntermidiateOps.java
@@ -1,4 +1,30 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.*;
+import java.util.stream.*;
+
 public class IntermidiateOps {
+    public static void main(String[] args) {
+        // 1. Filter
+        List<String> list = Arrays.asList("Luffy", "Zoro", "Sanji", "Ussop", "Chopper", "Franky", "Brook", "Robin", "Nami");
+        Stream<String> stream = list.stream().filter(x -> x.startsWith("L"));
+        long count = stream.count();
+
+        System.out.println(count);
+
+        // 2. map
+        Stream<String> stringStream = list.stream().map(String::toUpperCase);
+
+        // Sorted
+        Stream<String> sorted = stringStream.sorted((a,b) -> a.length() - b.length());
+        sorted.forEach(System.out::println);
+
+        // distinct
+
+        Stream<String> sorted2 = list.stream().sorted((a,b) -> a.length() - b.length()).distinct();
+        sorted2.forEach(System.out::println);
+
+
+    }
 }
