7e1bf80a0fef632668a1e691d8e8d1de9bad59c0
vrishabhparmar <vrishabhluvcars@gmail.com>
2025-09-29 23:13:16 +0530

    Primitive Streams

--- src/streams/PrimitiveStreams.java
+++ src/streams/PrimitiveStreams.java
@@ -1,4 +1,21 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
 public class PrimitiveStreams {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4};
+        IntStream stream = Arrays.stream(numbers);
+
+        // boxed converts IntStream to Wrapper stream
+        System.out.println(stream.boxed().collect(Collectors.toList()));
+
+        DoubleStream doubles = new Random().doubles(5);
+        System.out.println(doubles.boxed().toList());
+
+    }
 }
